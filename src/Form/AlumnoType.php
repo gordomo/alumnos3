42d22af3b48fa7eb218f81a15813cd4191504f82
gordomo <morimartin@gmail.com>
2022-05-03 15:46:42 -0300

    main - agrego validaciones en formularios

--- src/Form/AlumnoType.php
+++ src/Form/AlumnoType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,14 +26,14 @@ class AlumnoType extends AbstractType
             ->add('nombre', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('apellido', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('f_nac', DateType::class, ['widget' => 'single_text', 'html5' => true, 'attr' => ['class' => 'form-control']])
-            ->add('email', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('email', EmailType::class, ['attr' => ['class' => 'form-control']])
             ->add('l_nac', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('dni', NumberType::class, ['attr' => ['class' => 'form-control']])
             ->add('celular', NumberType::class, ['attr' => ['class' => 'form-control']])
             ->add('contacto_emergencia', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('n_tutor', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('t_tutor', NumberType::class, ['attr' => ['class' => 'form-control']])
-            ->add('corre_tutor', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('corre_tutor', EmailType::class, ['attr' => ['class' => 'form-control']])
             ->add('dni_tutor', NumberType::class, ['attr' => ['class' => 'form-control']])
             ->add('escuela', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('extras', TextType::class, ['attr' => ['class' => 'form-control']])
