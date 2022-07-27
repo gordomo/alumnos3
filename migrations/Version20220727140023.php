fa234086518543c0c689190d7e14302cae42ab77
gordomo <morimartin@gmail.com>
2022-07-27 14:52:36 -0300

    main - intento arreglar las migraciones

---
+++ migrations/Version20220727140023.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220727140023 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE alumno CHANGE f_nac f_nac DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE curso CHANGE duracion duracion DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE alumno CHANGE f_nac f_nac DATE NOT NULL');
+        $this->addSql('ALTER TABLE curso CHANGE duracion duracion INT NOT NULL');
+    }
+}
