3f017ef2e697cfe7a1e4bdda3612f3ccc8e0c4db
gordomo <morimartin@gmail.com>
2026-02-04 11:21:26 -0300

    version-3-saas - cambio timeout de login - cambio en landing - muestro usuario logueado en lsitado de usuarios

--- src/Controller/InstitutoUserController.php
+++ src/Controller/InstitutoUserController.php
@@ -46,7 +46,6 @@ class InstitutoUserController extends AbstractController
         $qb = $userRepository->createQueryBuilder('u')
             ->where('u.instituto = :instituto')
             ->setParameter('instituto', $instituto)
-            ->andWhere('u.id != :currentUserId')
             ->setParameter('currentUserId', $usuarioActual->getId())
             ->orderBy('u.email', 'ASC');
         
